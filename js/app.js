df516694c50631080a9ed2acdcf78d165f2d1eee
zmewa079 <zannatul.mewa07@gmail.com>
2021-10-15 18:33:30 -0400

    add cached elements

--- js/app.js
+++ js/app.js
@@ -2,10 +2,15 @@
 
 
 /*--------------------------------- Variables -------------------------------*/
-
+let quizButtons
 
 
 /*------------------------ Cached Element References ------------------------*/
+const popcultureQuizBtn = document.querySelector('#popCulture')
+const trueOrFalseQuizBtn = document.querySelector('#trueOrFalse')
+const funnyTriviaQuizBtn = document.querySelector('#funnyTrivia')
+const brandSlogansQuiz = document.querySelector('#brandSlogans')
+const randomQuizBtn = document.querySelector('#randomQuiz')
 
 
 
