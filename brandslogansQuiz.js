b3daf8a4ce523c456f57c369cc83a72097820c6f
zmewa079 <zannatul.mewa07@gmail.com>
2021-10-18 10:19:10 -0400

    add seperate js files that contain quiz material

---
+++ brandslogansQuiz.js
@@ -0,0 +1,23 @@
+function brandSlogans() {
+  brandSlogansQuiz = {
+    question1: "What brand does this slogan belong to: What's in your wallet?",
+    answerChoices: ['Chase', 'Citi', 'Discover', 'Capital One'],
+    rightAnswer: 3,
+    
+    question2: "What brand does this slogan belong to: Move fast and break things",
+    answerChoices: ["Instagram", 'Snapchat', "Facebook", "Twitter"],
+    rightAnswer: 2,
+    
+    question3: "What is the slogan for Google?",
+    answerChoices: ["Be what's next", "Don't be evil", "Think Different", "Go Beyond"],
+    rightAnswer: 1,
+    
+    question4: "What brand does this slogan belong to: Expect more pay less",
+    answerChoices: ['Walmart', 'Target', 'Walgreens', 'Trader Joes'],
+    rightAnswer: 1,
+    
+    question5: "What is the slogan for Adidas", 
+    answerChoices: ["Impossible is nothing", "Just do it", "Belong anywhere", "We go the extra mile"],
+    rightAnswer: 0
+  }
+}
---
+++ trueorfalseQuiz.js
@@ -0,0 +1,22 @@
+function trueOrFalse() {
+  trueOrFalseQuiz = {
+    question1: "In Scotland, the unicorn is their national animal.",
+    answerChoices: ['True', 'False'],
+
+    question2: "The Great Wall of China is visibe from space",
+    answerChoices: ['True', 'False'],
+    rightAnswer: 1,
+
+    question3: "An octopus has three hearts",
+    answerChoices: ['True', 'False'],
+    rightAnswer: 0,
+
+    question4: "In a regular deck of cards, all kings have a mustache.",
+    answerChoices: ['True', 'False'],
+    rightAnswer: 1,
+
+    question5: "Among the letters of the alphabet, only the letter J is not included in the periodic table.",
+    answerChoices: ['True', 'False'],
+    rightAnswer: 0,
+  }
+}
