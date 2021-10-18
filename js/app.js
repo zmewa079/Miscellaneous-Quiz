fa65199d9e3eb90b53b1493005cc12d6c4de5466
zmewa079 <zannatul.mewa07@gmail.com>
2021-10-18 09:44:32 -0400

    add event listeners

--- js/app.js
+++ js/app.js
@@ -2,13 +2,12 @@
 
 
 /*--------------------------------- Variables -------------------------------*/
-let quizButtons
-let trueOrFalse 
-let funnyTrivia 
-let brandSlogans
-let randomQuiz
-let question
-let pcQuiz
+// let trueOrFalse 
+// let funnyTrivia 
+// let brandSlogans
+// let randomQuiz
+// let question
+// let pcQuiz
 /*------------------------ Cached Element References ------------------------*/
 const popcultureQuizBtn = document.querySelector('#popCulture')
 const trueOrFalseQuizBtn = document.querySelector('#trueOrFalse')
@@ -16,16 +15,17 @@ const funnyTriviaQuizBtn = document.querySelector('#funnyTrivia')
 const brandSlogansQuiz = document.querySelector('#brandSlogans')
 const randomQuizBtn = document.querySelector('#randomQuiz')
 
-
-
 /*----------------------------- Event Listeners -----------------------------*/
-
+popcultureQuizBtn.addEventListener('click', handleclick)
+trueOrFalseQuizBtn.addEventListener('click', handleclick)
+funnyTriviaQuizBtn.addEventListener('click', handleclick)
+brandSlogansQuiz.addEventListener('click', handleclick)
+randomQuizBtn.addEventListener('click', handleclick)
 
 
 /*-------------------------------- Functions --------------------------------*/
 function quiz() {
   randomQuiz = [popcultureQuizBtn, trueOrFalseQuizBtn, funnyTriviaQuizBtn, brandSlogansQuiz]
-  
 }
 
 function popCulture() {
