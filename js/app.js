6aaef1fc3c520719e8327cd38500240f8be92332
zmewa079 <zannatul.mewa07@gmail.com>
2021-10-18 00:37:25 -0400

    format of first quiz complete

--- js/app.js
+++ js/app.js
@@ -3,8 +3,12 @@
 
 /*--------------------------------- Variables -------------------------------*/
 let quizButtons
-
-
+let trueOrFalse 
+let funnyTrivia 
+let brandSlogans
+let randomQuiz
+let question
+let pcQuiz
 /*------------------------ Cached Element References ------------------------*/
 const popcultureQuizBtn = document.querySelector('#popCulture')
 const trueOrFalseQuizBtn = document.querySelector('#trueOrFalse')
@@ -19,3 +23,34 @@ const randomQuizBtn = document.querySelector('#randomQuiz')
 
 
 /*-------------------------------- Functions --------------------------------*/
+function quiz() {
+  randomQuiz = [popcultureQuizBtn, trueOrFalseQuizBtn, funnyTriviaQuizBtn, brandSlogansQuiz]
+  
+}
+
+function popCulture() {
+  pcQuiz = {
+    question1: "How many Harry Potter films have been made?",
+    answerChoices: ['seven', 'nine', 'eight', 'six' ],
+    rightAnswer: 2,
+    points: 3,
+
+    question2: "What day is Star Wars day?",
+    answerChoices: ['October 3', 'May 4', 'January 9', 'July 4'],
+    rightAnswer: 1,
+    points: 3,
+
+    question3: "Who was the main character in Lord of the Rings?",
+    answerChoices: ["Gandalf", "Frodo", "Smeagol", "Bilbo Baggins"],
+    rightAnswer: 1,
+    points: 3,
+
+    // question4: 
+    // answerChoices:
+    // rightAnswer:
+    // points:
+  }
+}
+
+
+
